f4e1b3688489badf977eba14709e9fbdf43e5d18
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-05-12 23:02:21 +0530

    feat: test cases for unique()

---
+++ tests/unique.test.ts
@@ -0,0 +1,19 @@
+import {unique} from '../source';
+
+describe('Tests for unique()', () => {
+  it('should return empty array', () => {
+    expect(unique([])).toEqual([]);
+  });
+
+  it('should return unique array', () => {
+    expect(unique([1, 2, 2, 3, 4, 4, 5])).toEqual([1, 2, 3, 4, 5]);
+  });
+
+  it('should return unchanged array of empty objects', () => {
+    expect(unique([{}, {}])).toEqual([{}, {}]);
+  });
+
+  it('should return unchanged array of empty arrays', () => {
+    expect(unique([[], []])).toEqual([[], []]);
+  });
+});
