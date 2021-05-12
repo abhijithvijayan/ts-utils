1b87af8ea35cc355469e7d48fd736871a100ec00
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-05-12 22:20:53 +0530

    feat: test cases for randomNumberInRange()

---
+++ tests/randomNumberInRange.test.ts
@@ -0,0 +1,12 @@
+import {randomNumberInRange} from '../source';
+
+describe('Tests for randomNumberInRange()', () => {
+  it('should return a random number', () => {
+    const min = 10;
+    const max = 20;
+    const rand = randomNumberInRange(min, max);
+
+    expect(rand).toBeGreaterThan(min);
+    expect(rand).toBeLessThan(max);
+  });
+});
