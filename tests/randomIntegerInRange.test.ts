63178fe1e951d0c93588467c8a27bec8a62abefa
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-05-12 22:20:18 +0530

    feat: test cases for randomIntegerInRange()

---
+++ tests/randomIntegerInRange.test.ts
@@ -0,0 +1,12 @@
+import {randomIntegerInRange} from '../source';
+
+describe('Tests for randomIntegerInRange()', () => {
+  it('should return a random number', () => {
+    const min = 10;
+    const max = 20;
+    const rand = randomIntegerInRange(min, max);
+
+    expect(rand).toBeGreaterThanOrEqual(min);
+    expect(rand).toBeLessThan(max);
+  });
+});
