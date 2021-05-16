0a2b3bd380da45f4e46be084420fd1cd1fb64b38
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-05-17 00:25:55 +0530

    chore: add test case for splitArrayIntoChunks()
    Signed-off-by: abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>

--- tests/splitArrayIntoChunks.test.ts
+++ tests/splitArrayIntoChunks.test.ts
@@ -1,6 +1,10 @@
 import {splitArrayIntoChunks} from '../source';
 
 describe('Tests for splitArrayIntoChunks()', () => {
+  it('should return empty array for empty array', () => {
+    expect(splitArrayIntoChunks([], 3)).toEqual([]);
+  });
+
   it('should split array into sizes of 2', () => {
     expect(splitArrayIntoChunks([0, 1, 2, 3, 4], 2)).toEqual([
       [0, 1],
