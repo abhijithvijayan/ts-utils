dbf21380c65ff83a071f4e6c5f2f08221b399b71
abhijithvijayan <34790378+abhijithvijayan@users.noreply.github.com>
2021-06-21 01:06:12 +0530

    feat: new test cases for tryCatch() method

--- tests/tryCatch.test.ts
+++ tests/tryCatch.test.ts
@@ -23,4 +23,47 @@ describe('Tests for tryCatch()', () => {
       }),
     });
   });
+
+  test('should expect fulfilled and rejected states ', () => {
+    const ok = (arg1: string, arg2: string): string => {
+      return `${arg1}-${arg2}`;
+    };
+    const error = (arg1: string, arg2: string): void => {
+      throw new Error(ok(arg1, arg2));
+    };
+    let fn: Function;
+
+    expect(
+      (() => {
+        fn = tryCatch(ok);
+
+        return fn('no', 'error');
+      })()
+    ).toEqual({
+      status: FnExecStatus.FULFILLED,
+      value: 'no-error',
+    });
+
+    expect(
+      (() => {
+        fn = tryCatch(error);
+
+        return fn('throws', 'error');
+      })()
+    ).toEqual({
+      status: FnExecStatus.REJECTED,
+      reason: expect.objectContaining({
+        message: 'throws-error',
+      }),
+    });
+  });
+
+  it('should return undefined for void functions', () => {
+    const fn = tryCatch(() => {});
+
+    expect(fn()).toEqual({
+      status: FnExecStatus.FULFILLED,
+      value: undefined,
+    });
+  });
 });
